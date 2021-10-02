67a7db22b7950b7b85ea7a28fd3290fe219b34c4
JahonovAsilbek <asilbekjahonov2001@gmail.com>
2021-10-03 00:10:24 +0500

    ui added

--- app/src/main/java/uz/pdp/weatherapp/MapsActivity.kt
+++ app/src/main/java/uz/pdp/weatherapp/MapsActivity.kt
@@ -2,8 +2,8 @@ package uz.pdp.weatherapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
@@ -37,26 +37,33 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             longitude = it.longitude
             mMap.clear()
             val place = LatLng(it.latitude, it.longitude)
+            val camera = CameraUpdateFactory.newLatLngZoom(place, 5f)
             mMap.addMarker(MarkerOptions().position(place).title("Choosed place"))
-            Toast.makeText(this, "${it.latitude} ${it.longitude} ", Toast.LENGTH_SHORT).show()
+            mMap.animateCamera(camera)
         }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         // Add a marker in Sydney and move the camera
+        val tashkent = LatLng(41.2995, 69.2401)
+        val camera = CameraUpdateFactory.newLatLngZoom(tashkent, 5f)
+        mMap.moveCamera(camera)
+        mMap.animateCamera(camera)
         mapClick()
         getWeatherClick()
     }
 
     private fun getWeatherClick() {
         binding.getWeather.setOnClickListener {
-            val bundle = Bundle()
-            bundle.putDouble("latitude", latitude)
-            bundle.putDouble("longitude", longitude)
-            val intent = Intent(this, WeatherActivity::class.java)
-            intent.putExtras(bundle)
-            startActivity(intent)
+            if (latitude != 0.0 && longitude != 0.0) {
+                val bundle = Bundle()
+                bundle.putDouble("latitude", latitude)
+                bundle.putDouble("longitude", longitude)
+                val intent = Intent(this, WeatherActivity::class.java)
+                intent.putExtras(bundle)
+                startActivity(intent)
+            }
         }
     }
 }
--- app/src/main/java/uz/pdp/weatherapp/WeatherActivity.kt
+++ app/src/main/java/uz/pdp/weatherapp/WeatherActivity.kt
@@ -1,13 +1,16 @@
 package uz.pdp.weatherapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import uz.pdp.weatherapp.databinding.ActivityWeatherBinding
 import uz.pdp.weatherapp.view_model.WeatherViewModel
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class WeatherActivity : AppCompatActivity() {
 
@@ -17,6 +20,7 @@ class WeatherActivity : AppCompatActivity() {
     private var longitude = 0.0
     private var API_KEY = "fb2fb1d53560c78a716a3f74d82fba42"
 
+    @SuppressLint("SetTextI18n", "SimpleDateFormat")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWeatherBinding.inflate(layoutInflater)
@@ -25,12 +29,45 @@ class WeatherActivity : AppCompatActivity() {
         if (bundle != null) {
             latitude = bundle.getDouble("latitude")
             longitude = bundle.getDouble("longitude")
-            Toast.makeText(this, "$latitude $longitude", Toast.LENGTH_SHORT).show()
         } else Toast.makeText(this, "null", Toast.LENGTH_SHORT).show()
 
         weatherViewModel = ViewModelProvider(this).get(WeatherViewModel::class.java)
         weatherViewModel.getWeather(latitude, longitude, API_KEY).observe(this, Observer {
-            Log.d("AAAA", "$it")
+
+            binding.apply {
+                cityName.text = it.name
+
+                when (it.weather!![0].main) {
+                    "Clouds" -> cloud.setImageResource(R.drawable.ic_cloud)
+                    "Clear" -> cloud.setImageResource(R.drawable.ic_sun)
+                    "Rain" -> cloud.setImageResource(R.drawable.ic_rainy)
+                    "Snow" -> cloud.setImageResource(R.drawable.ic_snow)
+                    "Smoke" -> cloud.setImageResource(R.drawable.ic_smoke)
+                }
+
+                degreeMain.text = "${(it.main?.temp?.minus(273.15)?.toInt())}\u00B0"
+                status.text = it.weather!![0].main
+
+                val currentDate = Date(System.currentTimeMillis())
+                val simpleFormat = SimpleDateFormat("EEEE, MMMM d")
+                date.text = simpleFormat.format(currentDate).toString()
+
+                windSpeed.text = "${it.wind?.speed} m/s"
+                humidityPercent.text = "${it.main?.humidity} %"
+                maxTemp.text = "${(it.main?.temp_max?.minus(273.15)?.toInt())}\u00B0"
+                minTemp.text = "${(it.main?.temp_min?.minus(273.15)?.toInt())}\u00B0"
+
+                val date = Date((it.sys?.sunrise?.plus(it.timezone!!))!!.toLong())
+                val date1 = Date((it.sys?.sunset?.plus(it.timezone!!))!!.toLong())
+                val format = SimpleDateFormat("HH:mm").format(date)
+                val format1 = SimpleDateFormat("HH:mm").format(date1)
+
+                sunrise.text = format
+                sunset.text = format1
+
+                backBtn.setOnClickListener { finish() }
+            }
+
         })
     }
 }
